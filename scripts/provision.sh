af4b20c255226b1ed39c0d74d2caa04840c8d297
Avery Rozar <avery.rozar@critical-sec.com>
2020-09-16 13:55:28 -0400

    add dependencies

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,6 +2,6 @@
 
 sudo mv /tmp/fury.list /etc/apt/sources.list.d/
 sudo apt update
-sudo apt install ccscanner -y
+sudo apt install docker.io sysstat ccscanner -y
 sudo rm -rf /home/ubuntu/.ssh/authorized_keys
 sudo rm -rf /root/.ssh/authorized_keys
