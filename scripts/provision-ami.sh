135e23a8dbee15857682d8dd999143e091573c3d
Avery Rozar <avery.rozar@critical-sec.com>
2020-10-27 19:46:15 -0400

    add ansible remove vmware provision sh

--- scripts/provision-ami.sh
+++ scripts/provision-ami.sh
@@ -4,6 +4,6 @@ sudo chown root:root /tmp/sudoers
 sudo mv /tmp/sudoers /etc/sudoers
 sudo mv /tmp/fury.list /etc/apt/sources.list.d/
 sudo apt -qq update
-sudo apt -qq install docker.io sysstat openvpn ccscanner -y
+sudo apt -qq install docker.io sysstat openvpn ansible ccscanner -y
 sudo rm -rf /home/ubuntu/.ssh/authorized_keys
 sudo rm -rf /root/.ssh/authorized_keys
