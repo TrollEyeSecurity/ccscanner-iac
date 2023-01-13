d1713dfb620e3b14db0777ca6be47abd422a5285
Avery Rozar <avery.rozar@trolleyeseurity.com>
2023-01-13 18:16:31 -0500

    Update ccscanner to Ubuntu 22.04LTS

--- scripts/provision-ami.sh
+++ scripts/provision-ami.sh
@@ -2,8 +2,8 @@
 
 sudo chown root:root /tmp/sudoers
 sudo mv /tmp/sudoers /etc/sudoers
-sudo mv /tmp/fury.list /etc/apt/sources.list.d/
+sudo mv /tmp/trolleyesecurity.list /etc/apt/sources.list.d/
 sudo apt -qq update
-sudo apt -qq install docker.io sysstat openvpn ansible ccscanner -y
+sudo apt -qq install docker.io docker-compose sysstat openvpn ansible ccscanner -y
 sudo rm -rf /home/ubuntu/.ssh/authorized_keys
 sudo rm -rf /root/.ssh/authorized_keys
