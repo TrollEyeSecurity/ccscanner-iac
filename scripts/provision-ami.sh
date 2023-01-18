37e4aae9824d4a4dd4c0ca02e86bbfec6457d1a8
Avery Rozar <avery.rozar@trolleyeseurity.com>
2023-01-17 20:40:42 -0500

    update packages

--- scripts/provision-ami.sh
+++ scripts/provision-ami.sh
@@ -4,6 +4,6 @@ sudo chown root:root /tmp/sudoers
 sudo mv /tmp/sudoers /etc/sudoers
 sudo mv /tmp/trolleyesecurity.list /etc/apt/sources.list.d/
 sudo apt -qq update
-sudo apt -qq install docker.io docker-compose sysstat openvpn ansible ccscanner -y
+sudo apt -qq install open-vm-tools ccscanner -y
 sudo rm -rf /home/ubuntu/.ssh/authorized_keys
 sudo rm -rf /root/.ssh/authorized_keys
