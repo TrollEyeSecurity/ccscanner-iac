c3614232368571e4aa786e62bbe36f9da085f4e2
Avery Rozar <avery.rozar@trolleyesecurity.com>
2023-08-29 11:25:48 -0400

    update provision-ami.sh

--- scripts/provision-ami.sh
+++ scripts/provision-ami.sh
@@ -3,7 +3,7 @@
 sudo chown root:root /tmp/sudoers
 sudo mv /tmp/sudoers /etc/sudoers
 sudo mv /tmp/trolleyesecurity.list /etc/apt/sources.list.d/
-sudo apt -qq update
-sudo apt -qq install open-vm-tools ccscanner -y
+sudo apt update && sudo apt dist-upgrade -y
+sudo apt install open-vm-tools ccscanner -y
 sudo rm -rf /home/ubuntu/.ssh/authorized_keys
 sudo rm -rf /root/.ssh/authorized_keys
